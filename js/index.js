6858168e61113cf242026a99ffd1698bcd15ec18
anastl <anna.207346@gmail.com>
2022-09-28 15:34:22 -0400

    styles and functionalities + edge cases

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-const item = document.getElementById('item-to-add')
-const qtty = document.getElementById('qtty-to-add')
-const addBtn = document.getElementById('add-to-list')
-const listContainer = document.getElementById('list--container')
-
-let shoppingList = []
-
 class Items {
     constructor ( name, amount ) {
         this.name = name.toLowerCase()
@@ -32,14 +25,65 @@ class Items {
     }
 }
 
+const item = document.getElementById('item-to-add')
+const qtty = document.getElementById('qtty-to-add')
+const popUp = document.getElementById('pop-up')
+const addBtn = document.getElementById('add-to-list')
+const exportBtn = document.getElementById('export-btn')
+const listContainer = document.getElementById('list-container')
+
+let shoppingList = initializeArray()
+if ( shoppingList ) displayList()
+
 addBtn.addEventListener( 'click', addItem )
+exportBtn.addEventListener( 'click', exportList )
+item.addEventListener( 'keydown', pressEnter )
+qtty.addEventListener( 'keydown', pressEnter )
+
+async function exportList () {
+    try {
+        const toBeCopied = JSON.stringify( shoppingList )
+        await navigator.clipboard.writeText( toBeCopied )
+        popUp.classList.add('slide-in-bottom')
+        popUp.style.display = "initial"
+        setTimeout( () => { popUp.classList.add('slide-out-bottom') }, 1700 )
+        setTimeout( () => {
+            popUp.classList.remove('slide-in-bottom')
+            popUp.style.display = "none"
+        }, 2000)
+        popUp.classList.remove('slide-out-bottom')
+    } catch ( e ) { console.log( e ) }
+}
+
+function pressEnter( event ) {
+    if ( event.code === 'Enter' || event.code === 'NumpadEnter' ) 
+        addItem( event )
+}
+
+function initializeArray () {
+    const memoryArray = JSON.parse( localStorage.getItem('list') )
+    if ( ! memoryArray ) return []
+    return memoryArray.map( ( { name, amount } ) => ( new Items( name, amount ) ) )
+}
 
 function displayList () {
-    listContainer.innerHTML = shoppingList.map( item => item.getItemDiv() ).join('')
+    if ( shoppingList.length ) {
+        listContainer.innerHTML = shoppingList.map( item => item.getItemDiv() ).join('') 
+        
+        const total = shoppingList.reduce( ( counter, { amount } ) => counter+=amount, 0 )
+        const totalSpan = `<span class='total-items'>Total: ${total} elemento${total > 1 ? 's' : ''}</span>`
+        listContainer.innerHTML += totalSpan
+
+        localStorage.setItem( 'list', JSON.stringify( shoppingList ) )
+    } else {
+        listContainer.textContent = 'Ningún elemento ha sido agregado a tu lista de compras'
+        localStorage.clear()
+    }
 }
 
 function addItem ( event ) {
     event.preventDefault()
+    if ( item.value === "" || parseInt( qtty.value ) <= 0 || qtty.value === "" ) return
     const newItem = new Items( item.value, qtty.value )
     if ( shoppingList.every( ( { name } ) => name !== newItem.name ) ) {
         shoppingList = [ newItem, ...shoppingList ]
