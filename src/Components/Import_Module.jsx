771bb38a722265fbb92fc2252238d468650459a6
anastl <anna.207346@gmail.com>
2022-10-22 17:57:38 -0400

    fix imports

--- src/Components/Import_Module.jsx
+++ src/Components/Import_Module.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { importList } from '../index'
+import { importList } from '../../index'
 
 export default function Import_Module () {
     return (
         <>
-            <button id="import-btn" onClick={importList} className="import-btn text-btn">Importar</button>
+            <button id="import-btn" onClick={ importList } className="import-btn text-btn">Importar</button>
             <div id="import-container" className="import--container">
                 <textarea id="import-area" className="import-area" name="area para importar listas de compra" placeholder="Pegue aquí su lista de compras generada previamente"></textarea>
                 <button id="submit-list-btn" className="text-btn" >Enviar</button>
