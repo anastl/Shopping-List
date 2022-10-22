2df4d8b233dcd3e92958f55c0f4d4fd12e013678
anastl <anna.207346@gmail.com>
2022-10-22 17:25:37 -0400

    delete react.strictMode

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,4 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import '../css/styles.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')).render( <App /> )
